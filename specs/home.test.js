7161cab0e41c14557c95885f1e990a3b72a8a79a
jfugon <jennifer.guerrero@helloiconic.com>
2022-07-08 14:29:58 -0600

    test 5

--- specs/home.test.js
+++ specs/home.test.js
@@ -12,12 +12,12 @@ describe("home test", function () {
     expect(await browser.getTitle()).toEqual("Swag Labs");
     await percySnapshot(browser, "home page displayed");
   });
-
+  /*
   it("The user add products to the shopping cart", async function () {
     await browser.url("https://www.saucedemo.com/inventory.html");
     await sauceLabHomePage.ClickFirstProduct();
     await sauceLabHomePage.ClickSecondProduct();
     expect(await sauceLabHomePage.shoppingCartBadge.getText()).toEqual("2");
     await percySnapshot(browser, "Shopping cart badge increase");
-  });
+  });*/
 });
