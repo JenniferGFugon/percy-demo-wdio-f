904dfc48f34dde801e05f62fe94ed0a595af8c44
jfugon <jennifer.guerrero@helloiconic.com>
2022-07-08 17:58:32 -0600

    test

--- specs/car.test.js
+++ specs/car.test.js
@@ -1,1 +1,3 @@
-
+const percySnapshot = require("@percy/webdriverio");
+const sauceLabHomePage = require("../pages/sauceLab.home.page.js");
+const sauceLabLoginPage = require("../pages/sauceLab.login.page.js");
